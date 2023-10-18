93b84d1d48a3e7759f7067d17e92887a918c4d34
Abubakar Wazih Tushar <tushar.abubakar@gmail.com>
2023-10-18 06:13:41 +0530

    new

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,4 @@
 file { '/var/www/html/yourfile.txt':
-  ensure  => present,     # This ensures that the file exists
-  content => "This is the content of your file.\n",
+    ensure => present,
+    content => 'Hello, Puppet! updated',
 }
