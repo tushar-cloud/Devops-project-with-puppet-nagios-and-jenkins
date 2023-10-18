a85bfdddb8101f69b36bc73107e9dd39cd5bafde
Abubakar Wazih Tushar <tushar.abubakar@gmail.com>
2023-10-18 06:54:40 +0530

    new

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,4 @@
 file { '/var/www/html/index.html':
   ensure  => present,
-  content => "<html>\n<body>\n<h1>Hello, new Puppet!</h1>\n</body>\n</html>\n",
+  content => "<html>\n<body>\n<h1>Hello, new ddds Puppet!</h1>\n</body>\n</html>\n",
 }
